4ccf5b27de6afd0a8ad3e112477692d20cbdf044
gaoyanwang <gaoyanwang68@gmail.com>
2016-07-27 20:50:44 -0700

    20160727 #1,6,7,8,9 completed

--- Leetcode/001_Two_Sum.py
+++ Leetcode/001_Two_Sum.py
@@ -1,11 +1,12 @@
-def twoSum(nums, target):
-    if len(nums) > 1:
-        d = {}
-        for i in range(0, len(nums)):
-            if nums[i] not in d:
-                d[target - nums[i]] = nums.index(nums[i])
-            else:
-                return [i, d[nums[i]]]
-    return None
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) > 1:
+            d = {}
+            for i in range(0, len(nums)):
+                if nums[i] not in d:
+                    d[target - nums[i]] = i
+                else:
+                    return [i, d[nums[i]]]
+        return None
 # Time: O(n)
-# Space: O(1) or O(n)?
+# Space: O(n)
