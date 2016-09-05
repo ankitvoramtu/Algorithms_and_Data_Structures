5b92e36b1bf9b91d2c4bcaf723dff67e011f4731
gaoyanwang <gaoyanwang68@gmail.com>
2016-09-05 11:03:45 -0700

    #171 added

--- Leetcode/171_Excel_Sheet_Column_Number.py
+++ Leetcode/171_Excel_Sheet_Column_Number.py
@@ -1,1 +1,12 @@
-171. Excel Sheet Column Number
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ret = 0
+        for i in range(1, len(s) + 1):
+            dig = ord(s[-i]) - ord("A") + 1
+            cur = dig * 26 ** i
+            ret = ret + cur
+        return ret
