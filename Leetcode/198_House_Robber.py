ca6de093811c3185aced711e265949636531d3de
gaoyanwang <gaoyanwang68@gmail.com>
2016-09-17 11:32:46 -0700

    added questions

--- Leetcode/198_House_Robber.py
+++ Leetcode/198_House_Robber.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums) < 2:
+            return max(nums)
+
+        max1 = nums[0]
+        max2 = max(nums[0,]nums[1])
+        for i in range(2, len(nums)):
+            max_all = max(max1 + nums[i], max2)
+            max1 = max2
+            max2 = max_all
+        return max_all
         
--- Leetcode/202_Happy_Number.py
+++ Leetcode/202_Happy_Number.py
@@ -4,21 +4,22 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-    val1 = processed(n)
-    val2 = processed(processed(n))
+        val1, val2 = n, n
+        while True:
+            val1 = self.helper(val1)
+            val2 = self.helper(self.helper(val2))
+            if val2 == 1:
+                return True
+            if val2 == val1:
+                return False
 
-    while True:
-    	if  val2 == 1:
-    		return True
-    	if val2 == val1:
-    		return False
-    	val2 = processed(processed(n)):
-    	val1 = processed(n)
-
-	def processed(self,n):
-        s = str(n)
+    def helper(self, n):
         ret = 0
-        for i in range(0, len(s)):
-        	digit = int(s[i]) * int(s[i])
-        	ret = ret + digit
+        while n != 0:
+            num_square = (n % 10) * (n % 10)
+            ret = ret + num_square
+            n = n / 10
         return ret
+
+# Time: O(n)
+# Space: O(1)
