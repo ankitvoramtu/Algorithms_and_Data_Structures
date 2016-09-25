43aaa0b33d43bf7c81aa53bbab17bc67bc6564e1
gaoyanwang <gaoyanwang68@gmail.com>
2016-09-24 20:15:22 -0700

    Easy all updated

--- Leetcode/371_Sum_of_Two_Integers.py
+++ Leetcode/371_Sum_of_Two_Integers.py
@@ -5,13 +5,15 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        if a == 0:
-        	return b
-        if b == 0:
-        	return a
+        # 32 bits integer max
+        MAX = 0x7FFFFFFF
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
         while b != 0:
-        	carry = a & b
-        	a = a ^ b
-        	b = carry << 1
-        return a 
-        
+            # ^ get different bits and & gets double 1s, << moves carry
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
+        return a if a <= MAX else ~(a ^ mask)
+# Time: O(n)
+# Space: O(1)
